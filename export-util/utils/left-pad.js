02b325c96e57432771a752f60eb9b4689565fc5b
Oded Reuveny <oded@cocycles.com>
2017-10-30 19:14:20 +0200

    added jsdocs to example project

--- export-util/utils/left-pad.js
+++ export-util/utils/left-pad.js
@@ -1,8 +1,21 @@
+/**
+ * @description - Pad a string to the left.
+ * 
+ * @example
+ *  leftPad('foo', 5) // => "  foo"
+ *  leftPad('foobar', 6) // => "foobar"
+ *  leftPad(1, 2, '0') // => "01"
+ * 
+ * @param {string} str - string to pad
+ * @param {number} length - string length
+ * @param {string} add - characters to add as padding 
+ */
+
 function leftPad(str, length, add = " ") {
     let result = "";
     let string = str.toString();
     let lengthDiff = length - string.length;
-    if(lengthDiff > 0 ) {
+    if(lengthDiff > 0) {
         for(var i = 0; i< lengthDiff; i++) {
             result += add;
         }
