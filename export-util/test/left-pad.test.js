5f52abdf1b1d23288531ae6926fbcb86956f7c75
Oded Reuveny <oded@cocycles.com>
2017-10-31 18:32:41 +0200

    small changes to test

--- export-util/test/left-pad.test.js
+++ export-util/test/left-pad.test.js
@@ -1,9 +1,7 @@
 var assert = require('assert');
 var LeftPad = require('../utils/left-pad');
 describe('left-pad', function() {
-  describe('', function() {
     it('should return 000Bit', function() {
       assert.equal('000Bit', LeftPad('Bit',6,0));
     });
-  });
 });
